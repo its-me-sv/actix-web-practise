190b163c5b5ca3030dcd02e576b524b9b830a05b
Suraj Vijay <surajcpp115@gmail.com>
2023-05-29 20:40:34 +0530

    nesting

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,16 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+#[get("/suraj")]
+async fn suraj() -> impl Responder {
+    HttpResponse::Ok().body("Suraj Vijayan")
+}
+
+#[get("/monish")]
+async fn monish() -> impl Responder {
+    HttpResponse::Ok().body("Monish Vijayan")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
@@ -21,6 +31,7 @@ async fn main() -> std::io::Result<()> {
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
+            .service(web::scope("vijayans").service(suraj).service(monish))
     })
     .bind(("127.0.0.1", 5000))?
     .run()
