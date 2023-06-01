bc0816b03641ae360f0f5c76706afe36044d11ee
Suraj Vijay <surajcpp115@gmail.com>
2023-06-01 11:42:44 +0530

    extracting headers

--- src/extractors.rs
+++ src/extractors.rs
@@ -1,10 +1,10 @@
-use actix_web::Scope;
 use actix_web::{
     get,
     web::{Json, Path, Query},
     Result,
 };
-use serde::Deserialize;
+use actix_web::{HttpRequest, Scope};
+use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize)]
 struct ExtractorsKind {
@@ -27,9 +27,31 @@ async fn body_service(body: Json<ExtractorsKind>) -> Result<String> {
     Ok(format!("body: {} with {}", body.no, body.string))
 }
 
+#[derive(Serialize)]
+struct HeaderResponse {
+    auth: String,
+    custom: String,
+}
+
+#[get("/header")]
+async fn header_service(req: HttpRequest) -> Json<HeaderResponse> {
+    let auth_token = req
+        .headers()
+        .get("Authorization")
+        .unwrap()
+        .to_str()
+        .unwrap();
+    let custom_header = req.headers().get("x-custom").unwrap().to_str().unwrap();
+    Json(HeaderResponse {
+        auth: auth_token.to_owned(),
+        custom: custom_header.to_owned(),
+    })
+}
+
 pub fn extractors() -> Scope {
     actix_web::web::scope("/extractors")
         .service(path_service)
         .service(query_service)
         .service(body_service)
+        .service(header_service)
 }
